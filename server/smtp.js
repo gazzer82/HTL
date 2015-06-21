bc0a68137518518a40cefdb7fac0871effe86f09
gazzer82 <contact@garethjeanne.co.uk>
2015-06-20 20:37:05 -0700

    added from email address for meteor-admin

--- server/smtp.js
+++ server/smtp.js
@@ -4,7 +4,7 @@ Meteor.startup(function () {
     username: 'postmaster@dcloud.space',   // eg: server@gentlenode.com
     password: '1b65fec55d83fd010f9e98dbdc8d5b67',   // eg: 3eeP1gtizk5eziohfervU
     server:   'smtp.mailgun.org',  // eg: mail.gandi.net
-    port: 25
+    port: 587
   };
 
   process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
