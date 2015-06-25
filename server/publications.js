15fc3db341d3c3decd0c6271c7a5ebb0d759053a
gazzer82 <contact@garethjeanne.co.uk>
2015-06-25 01:04:20 -0500

    added smart collections to spead up page loading

--- server/publications.js
+++ server/publications.js
@@ -10,6 +10,42 @@ Meteor.publish('socialPosts', function(filter, limit, roles) {
     return postsReturn;
 });
 
+Meteor.publish('socialPostsNew', function(limit, roles) {
+    var postsReturn = [];
+    if (this.userId && Roles.userIsInRole(this.userId, ['admin'])){
+      postsReturn = socialPosts.find({postStatus: filter}, {postDate: -1}, { limit: limit });
+    } else if (this.userId){
+      RolesList = Roles.getRolesForUser(this.userId);
+      postsReturn = socialPosts.find({ $and: [{postStatus: 'new'},{postEventName: {$in: RolesList}}]}, {postDate: -1}, { limit: limit });
+    }
+    postsReturn;
+    return postsReturn;
+});
+
+Meteor.publish('socialPostsDeleted', function(limit, roles) {
+    var postsReturn = [];
+    if (this.userId && Roles.userIsInRole(this.userId, ['admin'])){
+      postsReturn = socialPosts.find({postStatus: deleted}, {postDate: -1}, { limit: limit });
+    } else if (this.userId){
+      RolesList = Roles.getRolesForUser(this.userId);
+      postsReturn = socialPosts.find({ $and: [{postStatus: 'deleted'},{postEventName: {$in: RolesList}}]}, {postDate: -1}, { limit: limit });
+    }
+    postsReturn;
+    return postsReturn;
+});
+
+Meteor.publish('socialPostsApproved', function(limit, roles) {
+    var postsReturn = [];
+    if (this.userId && Roles.userIsInRole(this.userId, ['admin'])){
+      postsReturn = socialPosts.find({postStatus: 'approved'}, {postDate: -1}, { limit: limit });
+    } else if (this.userId){
+      RolesList = Roles.getRolesForUser(this.userId);
+      postsReturn = socialPosts.find({ $and: [{postStatus: approved},{postEventName: {$in: RolesList}}]}, {postDate: -1}, { limit: limit });
+    }
+    postsReturn;
+    return postsReturn;
+});
+
 Meteor.publish('users', function(filter) {
   if (this.userId && Roles.userIsInRole(this.userId, ['admin'])) {
       return  users.find();
