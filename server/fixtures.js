9421ecefaf273e3f2df7edacc45000c26bb9e50e
gazzer82 <contact@garethjeanne.co.uk>
2015-07-11 14:21:41 -0700

    added subscripe functions for ios app

--- server/fixtures.js
+++ server/fixtures.js
@@ -38,3 +38,53 @@ if(Meteor.users.find().count() === 0) {
 var user = Meteor.users.findOne();
 
 Roles.addUsersToRoles(user._id, 'admin', Roles.GLOBAL_GROUP);
+
+
+if (Lists.find().count() === 0) {
+    var data = [
+      {name: "Meteor Principles",
+       items: ["Data on the Wire",
+         "One Language",
+         "Database Everywhere",
+         "Latency Compensation",
+         "Full Stack Reactivity",
+         "Embrace the Ecosystem",
+         "Simplicity Equals Productivity"
+       ]
+      },
+      {name: "Languages",
+       items: ["Lisp",
+         "C",
+         "C++",
+         "Python",
+         "Ruby",
+         "JavaScript",
+         "Scala",
+         "Erlang",
+         "6502 Assembly"
+         ]
+      },
+      {name: "Favorite Scientists",
+       items: ["Ada Lovelace",
+         "Grace Hopper",
+         "Marie Curie",
+         "Carl Friedrich Gauss",
+         "Nikola Tesla",
+         "Claude Shannon"
+       ]
+      }
+    ];
+
+    var timestamp = (new Date()).getTime();
+    _.each(data, function(list) {
+      var list_id = Lists.insert({name: list.name,
+        incompleteCount: list.items.length});
+
+      _.each(list.items, function(text) {
+        Todos.insert({listId: list_id,
+                      text: text,
+                      createdAt: new Date(timestamp)});
+        timestamp += 1; // ensure unique timestamp.
+      });
+    });
+}
--- server/publications.js
+++ server/publications.js
@@ -1,16 +1,18 @@
-/*Meteor.publish('socialPosts', function(filter, limit, roles) {
+Meteor.publish('socialPostsiOS', function(limit) {
     var postsReturn = [];
+    RolesList = Roles.getRolesForUser(this.userId);
     if (this.userId && Roles.userIsInRole(this.userId, ['admin'])){
-      postsReturn = socialPosts.find({postStatus: filter}, {postDate: -1}, { limit: limit });
+      //postsReturn = socialPosts.find({sort: {postDate:-1}});
+      postsReturn = socialPosts.find({sort: {postDate:-1}});
     } else if (this.userId){
-      RolesList = Roles.getRolesForUser(this.userId);
-      postsReturn = socialPosts.find({ $and: [{postStatus: filter},{postEventName: {$in: RolesList}}]}, {postDate: -1}, { limit: limit });
+      postsReturn = socialPosts.find({postEventName: {$in: RolesList}}, {sort: {postDate:-1}});
     }
-    postsReturn;
-    return postsReturn;
-});*/
+    //return postsReturn;
+    return socialPosts.find();
+});
 
 Meteor.publish('socialPosts', function() {
+    //return socialPosts.find();
     return socialPosts.find();
 });
 
@@ -34,15 +36,37 @@ Meteor.publish("HTLEvents", function() {
 
 Meteor.methods({
   approvePost: function (postID) {
+    console.log('approving post');
     var loggedInUser = Meteor.user();
     if (loggedInUser) {
       socialPosts.update(postID, {$set: {postStatus: "approved"}});
     }
   },
   deletePost: function (postID) {
+    console.log('deleting post');
     var loggedInUser = Meteor.user();
     if (loggedInUser) {
     socialPosts.update(postID, {$set: {postStatus: "deleted"}});
     }
   }
+});
+
+Meteor.publish('publicLists', function() {
+  if (this.userId && Roles.userIsInRole(this.userId, ['admin'])) {
+    return Lists.find({userId: {$exists: false}});
+  }
+});
+
+Meteor.publish('privateLists', function() {
+  if (this.userId) {
+    return Lists.find({userId: this.userId});
+  } else {
+    this.ready();
+  }
+});
+
+Meteor.publish('todos', function(listId) {
+  check(listId, String);
+
+  return Todos.find({listId: listId});
 });
